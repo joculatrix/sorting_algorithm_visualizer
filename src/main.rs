ffc58478ba202fe7d351e114cd98a520b2587fa2
Sloane Vaznova <sloane.vaznova@gmail.com>
2024-09-25 16:47:40 -0500

    Add ratatui boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+use std::{error::Error, io};
+
+use ratatui::{
+    crossterm::{
+        event::{DisableMouseCapture, EnableMouseCapture},
+        execute,
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
+    },
+    prelude::CrosstermBackend,
+    Terminal
+};
+
 mod sort;
 
 pub const ARRAY_LEN: usize = 10;
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn Error>> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // run app
+
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture)?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
