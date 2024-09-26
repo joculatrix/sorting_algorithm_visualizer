767fd6a19ba0864044bd8428e4fca4a5a2cae7ed
Sloane Vaznova <sloane.vaznova@gmail.com>
2024-09-25 19:20:46 -0500

    Build on menu UI

--- src/app.rs
+++ src/app.rs
@@ -7,25 +7,50 @@ pub enum AppScreen {
     Sort,
 }
 
+pub struct Algorithm {
+    pub name: &'static str,
+    pub new: fn() -> Box<dyn Sort<usize>>,
+}
+
 pub struct App {
     pub current_screen: AppScreen,
-    pub algorithms: Vec<(&'static str, fn() -> Box<dyn Sort<usize>>)>,
+    pub algorithms: Vec<Algorithm>,
+    pub selected: usize,
 }
 
 impl App {
     pub fn new() -> App {
-        let mut algorithms: Vec<(&str, fn() -> Box<dyn Sort<usize>>)> = vec![
-                ("Bogosort", || Box::new(BogoSort::new())),
-                ("Bubble sort", || Box::new(BubbleSort::new())),
-                ("Cocktail shaker sort", || Box::new(CocktailShakerSort::new())),
-                ("Insertion sort", || Box::new(InsertionSort::new())),
-                ("Quicksort", || Box::new(QuickSort::new())),
-                ("Selection sort", || Box::new(SelectionSort::new())),   
+        let mut algorithms: Vec<Algorithm> = vec![
+                Algorithm {
+                    name: "Bogosort",
+                    new: || Box::new(BogoSort::new())
+                },
+                Algorithm {
+                    name: "Bubble sort",
+                    new: || Box::new(BubbleSort::new())
+                },
+                Algorithm {
+                    name: "Cocktail shaker sort",
+                    new: || Box::new(CocktailShakerSort::new())
+                },
+                Algorithm {
+                    name: "Insertion sort",
+                    new: || Box::new(InsertionSort::new())
+                },
+                Algorithm {
+                    name: "Quicksort",
+                    new: || Box::new(QuickSort::new())
+                },
+                Algorithm {
+                    name: "Selection sort",
+                    new: || Box::new(SelectionSort::new())
+                },
         ];
 
         App {
             algorithms,
             current_screen: AppScreen::Menu,
+            selected: 0,
         }
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,32 +1,87 @@
-use ratatui::{layout::{Constraint, Direction, Layout}, style::{Color, Style}, text::Span, widgets::{List, ListItem}, Frame};
+use ratatui::{
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style::{Color, Style},
+    text::Span,
+    widgets::{Block, Borders, List, ListItem, Paragraph},
+    Frame
+};
 
-use crate::app::App;
+use crate::app::{App, AppScreen};
 
 pub fn ui(frame: &mut Frame, app: &App) {
-    match app.current_screen {
-        crate::app::AppScreen::Menu => render_menu(frame, app),
-        crate::app::AppScreen::Sort => todo!(),
-    }
-    
-}
-
-fn render_menu(frame: &mut Frame, app: &App) {
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
             Constraint::Length(3),
             Constraint::Min(1),
+            Constraint::Length(3),
         ])
         .split(frame.area());
 
-    let list = List::new(
-        app.algorithms.iter().map(|alg|
-            ListItem::new(
-                Span::styled(alg.0, Style::default().fg(Color::Red))
-            )
-        )
-        .collect::<Vec<ListItem>>()
-    );
+    // TITLE
 
-    frame.render_widget(list, chunks[1]);
+    let title_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+
+    let title = Paragraph::new("Sorting Algorithm Visualizer")
+        .alignment(Alignment::Center)
+        .block(title_block);
+
+    frame.render_widget(title, chunks[0]);
+
+    // CONTENT
+
+    let content_area = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage(20),
+            Constraint::Percentage(60),
+            Constraint::Percentage(20),
+        ])
+        .split(chunks[1])[1];
+
+    match app.current_screen {
+        AppScreen::Menu => render_menu(frame, content_area, app),
+        AppScreen::Sort => todo!(),
+    }
+
+    // FOOTER
+
+    let footer_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+
+    let footer = Paragraph::new(
+        match app.current_screen {
+            AppScreen::Menu => "<Enter> to confirm, <Esc> to quit",
+            AppScreen::Sort => "<Esc> to return",
+        }
+    ).alignment(Alignment::Center).block(footer_block);
+
+    frame.render_widget(footer, chunks[2]);
+    
+}
+
+fn render_menu(frame: &mut Frame, area: Rect, app: &App) {
+    let mut list_items = vec![];
+
+    for i in 0..app.algorithms.len() {
+        list_items.push(
+            ListItem::new(
+                Span::styled(
+                    app.algorithms[i].name,
+                    if i == app.selected {
+                        Style::default().bg(Color::Red).fg(Color::Black)
+                    } else {
+                        Style::default().fg(Color::Red)
+                    }
+                )
+            )
+        );
+    }
+
+    let list = List::new(list_items);
+
+    frame.render_widget(list, area);
 }
